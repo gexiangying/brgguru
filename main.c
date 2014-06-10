97514950c68f577b3f971f0733942b64e6194aba
gexiangying <gexiangying@foxmail.com>
2014-06-10 10:51:18 +0800

    add coumpter

--- main.c
+++ main.c
@@ -5,12 +5,22 @@
 #include "lualib.h"
 #include "lauxlib.h"
 HINSTANCE g_hinst = NULL;
+static int get_computer_name(lua_State* L)
+{
+	TCHAR name[256];
+  DWORD len = 255;
+	BOOL bOK = GetComputerName(&name[0],&len);
+	lua_pushstring(L,name);
+	return 1;
+}
 int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, // 
 		   LPSTR lpCmdLine, int nCmdShow) // 
 {				// 
 	g_hinst = hinstance;
 	lua_State* L = lua_open();	
 	luaL_openlibs(L);
+	lua_pushcfunction(L,&get_computer_name);
+	lua_setglobal(L,"get_computer_name");
 	if(luaL_dofile(L,"main.lua") != 0){
 		const char* error_str = lua_tostring(L,-1);
 		MessageBox(NULL,error_str,"Error",MB_OK);
